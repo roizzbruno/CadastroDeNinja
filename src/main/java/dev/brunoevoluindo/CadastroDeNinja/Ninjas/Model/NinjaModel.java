53e1ad333a452e2ae78ae798734d41d8e5c329d2
Bruno <rodriguess.brunno1@gmail.com>
2026-08-16 14:33:15 -0300

    fix: Torna a coluna de email como unique + Adiciona anotações de estudos sobre as notações.

--- src/main/java/dev/brunoevoluindo/CadastroDeNinja/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/brunoevoluindo/CadastroDeNinja/Ninjas/Model/NinjaModel.java
@@ -8,9 +8,15 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-//Entity transforma uma classe em uma entidade do BD.
-//JPA = Java Persistence API
-
+// Entity transforma uma classe em uma entidade do BD.
+// JPA = Java Persistence API
+// Table(name = "  ") - Dá um nome para a tabela
+/* NoArgsConstructor - Adiciona um construtor vazio
+que irá ser atualizado automaticamente pelo Lombok. */
+/* AllArgsConstructor - Adiciona um construtor completo
+que irá ser atualizado automaticamente pelo Lombok. */
+/* Data - Adiciona todos os Getters e Setters de cada atributo
+e será atualizado automaticamente pelo Lombok. */
 @Entity
 @Table(name = "tb_cadastro")
 @NoArgsConstructor
@@ -18,13 +24,18 @@ import java.util.List;
 @Data
 public class NinjaModel {
 
+    /* Id + GeneratedValue(strategy = GenerationType.IDENTITY) _
+    o atributo ID irá ser atribuído sequencialmente (1, 2, 3, ...) */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
 
-    private String cla;
+    /* Column(unique = true) - essa coluna não pode ter itens repetidos.
+    Utilizado em cpf, rg, n_passaporte, ou seja, dados ÚNICOS de cada pessoa. */
+    @Column(unique = true)
+    private String email;
 
     private int idade;
 
