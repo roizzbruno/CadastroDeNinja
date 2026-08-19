e496852674619439c77ab421108e3c7b7f5d03ea
Bruno <rodriguess.brunno1@gmail.com>
2026-08-18 21:17:56 -0300

    feat: Alterar rota do controller para listar todos os ninjas; Injeta a dependência do SERVICE e do REPOSITORY.

--- src/main/java/dev/brunoevoluindo/CadastroDeNinja/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/brunoevoluindo/CadastroDeNinja/Ninjas/Controller/NinjaController.java
@@ -1,14 +1,55 @@
 package dev.brunoevoluindo.CadastroDeNinja.Ninjas.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.brunoevoluindo.CadastroDeNinja.Ninjas.Model.NinjaModel;
+import dev.brunoevoluindo.CadastroDeNinja.Ninjas.Services.NinjaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
     @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Essa é a minha primeira mensagem nessa rota.";
     }
+
+    //Adicionar Ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    //Procurar Ninja por ID (CREATE)
+    @GetMapping("/listarNinjasID")
+    public String mostrarNinjaPorId() {
+        return "Lista de Ninjas por ID";
+    }
+
+    //Mostrar todos os Ninjas(READ)
+    @GetMapping("/listarNinjas")
+    public List<NinjaModel> listarNinja() {
+        return ninjaService.listarNinjas();
+    }
+
+    //Alterar dados dos Ninjas (UPDATE)
+    @PutMapping("/alterarPorID")
+    public String alterarNinjaPorID() {
+        return "Ninja atualizado";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarNinjaPorID")
+    public String deletarNinjaPorID() {
+        return "Ninja deletado";
+    }
+
+
 }
--- src/main/java/dev/brunoevoluindo/CadastroDeNinja/Ninjas/Services/NinjaService.java
+++ src/main/java/dev/brunoevoluindo/CadastroDeNinja/Ninjas/Services/NinjaService.java
@@ -1,4 +1,23 @@
 package dev.brunoevoluindo.CadastroDeNinja.Ninjas.Services;
 
+import dev.brunoevoluindo.CadastroDeNinja.Ninjas.Model.NinjaModel;
+import dev.brunoevoluindo.CadastroDeNinja.Ninjas.Repository.NinjaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    //Injeção da dependência NinjaRepository
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //Serviços/Funções
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
 }
